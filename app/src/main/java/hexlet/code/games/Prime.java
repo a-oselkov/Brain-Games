43f748b6596097fb13ebfd2de4c3f6387a1a2cfb
a-oselkov <a.oselkov@gmail.com>
2023-01-31 19:16:43 +0300

    new version of the project

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -6,7 +6,7 @@ public class Prime {
     public static String playPrime() {
         int number = generateRandomNumber(0, 100);
         String rightAnswer;
-        System.out.println("Question " + number);
+        System.out.println("Question: " + number);
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 rightAnswer = "no";
