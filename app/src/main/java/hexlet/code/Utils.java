fdb0b572173fde2ebfdfcf407f5111de7470e82d
a-oselkov <a.oselkov@gmail.com>
2023-02-01 19:08:19 +0300

    edit project

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,14 +1,12 @@
 package hexlet.code;
 
 public class Utils {
+    public static final int MAX_NUMBER = 100;
     public static int generateRandomNumber(int min, int max) {   //Генератор случайного числа в диапозоне [min ; max]
         max = max - min;
         return (int) (Math.random() * (max + 1) + min);
     }
     public static int generateRandomNumber() {   //Генератор случайного числа в диапозоне [min ; max]
-        int min = 0;
-        int max = 100;
-        max = max - min;
-        return (int) (Math.random() * (max + 1) + min);
+        return (int) (Math.random() * (MAX_NUMBER + 1));
     }
 }
