27ab65fc2b46b1de5addef75cfb604defd1ef97e
a-oselkov <a.oselkov@gmail.com>
2023-03-25 19:21:59 +0300

    update menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,15 +11,16 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
-        System.out.print("Your choice: ");
+        System.out.print(String.format("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit
+                Your choice:\s"""));
 
         Scanner scanner = new Scanner(System.in);
         System.out.println(" ");
@@ -38,3 +39,4 @@ public class App {
         scanner.close();
     }
 }
+
