e03ac684eb6401f53263651103a68e7241127c23
a-oselkov <a.oselkov@gmail.com>
2023-03-25 15:59:49 +0300

    some edit

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,26 +4,28 @@ import java.util.Scanner;
 
 public class Engine {
     public static final int MAX_ROUNDS = 3;
+
     public static void playGame(String rules, String[][] questionsAnswersGame) {
         String playerName = Greeting.greet();
 
         System.out.println(rules);
 
-        for (int i = 0; i < MAX_ROUNDS; i++) {
-            System.out.print("Question: ");
-            System.out.println(questionsAnswersGame[i][0]);
-            System.out.print("Your answer: ");
+        for (String[] round : questionsAnswersGame) {
+            System.out.print(String.format("""
+                    Question: %s
+                    Your answer:\s""", round[0]));
             Scanner scanner = new Scanner(System.in);
             String answer = scanner.nextLine();
-            if (!answer.equals(questionsAnswersGame[i][1])) {
-                System.out.print("'" + answer + "'" + " is wrong answer ;(. ");
-                System.out.println("Correct answer was " + "'" + questionsAnswersGame[i][1] + "'.");
-                System.out.println("Let's try again, " + playerName + "!");
+            if (!answer.equals(round[1])) {
+                System.out.println(String.format("""
+                        '%s' is wrong answer ;(. Correct answer was '%s'.
+                        Let's try again, %s!""", answer, round[1], playerName));
                 return;
             }
             System.out.println("Correct!");
         }
-        System.out.println("Congratulations, " + playerName + "!");
+        System.out.println(String.format("Congratulations, %s!", playerName));
     }
 }
+
 
--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -42,3 +42,4 @@ public class Calc {
         Engine.playGame(CALC_RULES, generateGameQuestion());
     }
 }
+
